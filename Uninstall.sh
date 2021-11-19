453d2a351158c50140ba42b04fdca88f07591d7e
Eliot <77872672+Elliot290@users.noreply.github.com>
2021-11-19 10:57:23 +0530

    Delete Uninstall.sh
    B

--- Uninstall.sh
+++
@@ -1,6 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash
-
-rm $PREFIX/bin/login.py
-cp old_login $PREFIX/bin/login
-chmod 700 $PREFIX/bin/login
-echo Remove Pass By BaapG. Follow Me On Insta @krish_na_2568
