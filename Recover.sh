5e5da69e81b782bb9aa997a165f02e77b74c0f7c
Eliot <77872672+Elliot290@users.noreply.github.com>
2021-11-19 10:56:23 +0530

    Create Recover.sh
    B

---
+++ Recover.sh
@@ -0,0 +1,26 @@
+cd $HOME
+cd /sdcard
+mv .Telegram Telegram
+mv .browser browser
+mv .GBWhatsApp GBWhatsApp
+mv .BGMI BGMI
+mv .ApkEditor ApkEditor
+mv .Download Download
+mv .Instagram Instagram
+mv .SHAREit SHAREit
+mv .bluetooth bluetooth
+mv .WhatsApp WhatsApp
+mv .VidMate VidMate
+mv .Instagram Instagram
+mv .YoWhatsApp YoWhatsApp
+mv .DCIM DCIM
+mv .GBWhatsApp GBWhatsApp
+mv .Android Android
+mv .documents documents
+mv .IMO IMO
+mv .Movies Movies
+mv .MT2 MT2
+mv .WhatsApp WhatsApp
+mv .sketchware sketchware
+mv .Pictures Pictures
+mv .YoWhatsApp YoWhatsApp
