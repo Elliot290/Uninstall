f482998fb4d1e1dd95ff0e6c5047869bebd7f78d
Eliot <77872672+Elliot290@users.noreply.github.com>
2021-11-20 08:33:54 +0530

    Update Recover.sh
    B

--- Recover.sh
+++ Recover.sh
@@ -1,26 +1,31 @@
 cd $HOME
 cd /sdcard
+mv Telegram BaapG-Telegram
 mv .Telegram Telegram
 mv .browser browser
 mv .GBWhatsApp GBWhatsApp
 mv .BGMI BGMI
 mv .ApkEditor ApkEditor
+mv Download BaapG-Download
 mv .Download Download
+mv Instagram Instagram2
 mv .Instagram Instagram
 mv .SHAREit SHAREit
 mv .bluetooth bluetooth
-mv .WhatsApp WhatsApp
 mv .VidMate VidMate
-mv .Instagram Instagram
 mv .YoWhatsApp YoWhatsApp
+mv DCIM BAAPG-DCIM
 mv .DCIM DCIM
 mv .GBWhatsApp GBWhatsApp
+mv Android Android-BaapG
 mv .Android Android
 mv .documents documents
 mv .IMO IMO
 mv .Movies Movies
 mv .MT2 MT2
+mv WhatsApp BaapG-WhatsApp
 mv .WhatsApp WhatsApp
 mv .sketchware sketchware
+mv Pictures BaapG-Picture
 mv .Pictures Pictures
 mv .YoWhatsApp YoWhatsApp
